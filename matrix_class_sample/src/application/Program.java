8d8be21b6064b0ce08f93ab3b7320ce88e45b25b
Icaro Camillo <icarocamillo@gmail.com>
2022-01-19 15:53:21 -0300

    Rename and Format

--- matrix_class_sample/src/application/Program.java
+++ matrix_class_sample/src/application/Program.java
@@ -22,7 +22,7 @@ public class Program {
          vect[i] = new Product(name, result);
       }
 
-      double sum = 0.0D;
+      double sum = 0.0;
 
       for(int i = 0; i < vect.length; ++i) {
          sum += vect[i].getPrice();
