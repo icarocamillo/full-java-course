a218c30c87f8f3a83913e87adb917875fa33cb37
Icaro Camillo <icarocamillo@gmail.com>
2022-02-06 20:47:03 -0300

    Samples, Polymorphism, Inheritance, Downcasting, Upcasting and Sobreposition

---
+++ sample_sobreposition_override/src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Account acc1 = new Account(1001, "Alex", 1000.0);
+		acc1.withdraw(200.0);
+		System.out.println(acc1.getBalance());
+		
+		//Sobreposition sample using withdraw method from Savings Account
+		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+		acc2.withdraw(200.0);
+		System.out.println(acc2.getBalance());
+		
+		//Sobreposition sample using withdraw with super from Account to Business Account 
+		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+		acc3.withdraw(200.0);
+		System.out.println(acc3.getBalance());
+	}
+}
